cd8c1b573888c0073c078344a5d116f4d901daa7
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-07-30 11:05:00 -0300

    Aula 'useMemo' (C5-M2-SM2)

--- src/components/FriendsList.tsx
+++ src/components/FriendsList.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useMemo } from 'react'
+import { View, Text } from 'react-native'
 import { Friend } from './Friend'
 
 interface IFriendsListProps {
@@ -13,8 +13,18 @@ interface IFriendsListProps {
 export function FriendsList({
   friends
 }: IFriendsListProps) {
+  const totalFollowers = useMemo(() => {
+    return friends.reduce((followers, friend) => {
+      return followers + friend.followers
+    }, 0)
+  }, [friends])
+  
   return (
     <View>
+      <Text>
+        Total de seguidores: {totalFollowers}
+      </Text>
+
       {
         friends.map(friend => (
           <Friend
